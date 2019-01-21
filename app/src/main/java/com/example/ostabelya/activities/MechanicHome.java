b8da10f564f08ceda84df55a973a046df62eb388
Zakyyy <ahmed.abdelghafour@student.guc.edu.eg>
2019-01-21 10:20:44 +0200

    database done + MechanicHomeActivity

---
+++ app/src/main/java/com/example/ostabelya/activities/MechanicHome.java
@@ -0,0 +1,29 @@
+package com.example.ostabelya.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import com.example.ostabelya.R;
+
+public class MechanicHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mechanic_home);
+    }
+
+//    public void scheduleButtonPressed(View homeScreenView){
+//        startActivity(new Intent(this, ));
+//    }
+//    public void workersButtonPressed(View homeScreenView){
+//        startActivity(new Intent(this, ));
+//    }
+//    public void payButtonPressed(View homeScreenView){
+//        startActivity(new Intent(this, ));
+//    }
+//    public void buyCarPartsButtonPressed(View homeScreenView){
+//        startActivity(new Intent(this, ));
+//    }
+}
