e989ea06159e2382f58cbbf4cb2c5c4761a54eac
Ahmed Hisham Rihan <36399893+ahmed1hisham@users.noreply.github.com>
2019-01-21 17:25:15 +0200

    Adjusted worker model

--- app/src/main/java/com/example/ostabelya/activities/EmalaActivity.kt
+++ app/src/main/java/com/example/ostabelya/activities/EmalaActivity.kt
@@ -4,22 +4,30 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import com.example.ostabelya.R
 import com.example.ostabelya.adapters.EmalaAdapter
+import com.example.ostabelya.adapters.MechanicPaymentsAdapter
+import com.example.ostabelya.firebase.FirebaseUtils
+import com.example.ostabelya.models.Transaction
 import com.example.ostabelya.models.Worker
+import com.example.ostabelya.toaster
 import kotlinx.android.synthetic.main.activity_emala.*
+import java.util.ArrayList
 
 class EmalaActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emala)
 
-        val workers = arrayListOf<Worker>()
-
-        for (i in 1..100) {
-            workers.add(Worker("محمد جمال", 2012565583, "", "", 100))
-        }
+        var workers = arrayListOf<Worker>()
+        FirebaseUtils.getMechanicWorkers({ response ->
+            workers = response!!
+            Unit
+        }, {
+            toaster("No Authenticated user", Toast.LENGTH_SHORT)
+            Unit
+        })
 
         recycler_view.apply {
             layoutManager = GridLayoutManager(this@EmalaActivity, 2)
--- app/src/main/java/com/example/ostabelya/models/Worker.kt
+++ app/src/main/java/com/example/ostabelya/models/Worker.kt
@@ -2,7 +2,6 @@ package com.example.ostabelya.models
 
 data class Worker (val name: String,
                    val accountNumber: Int,
-                   val username: String,
                    val imgURL: String,
                    val salary: Int
 )
