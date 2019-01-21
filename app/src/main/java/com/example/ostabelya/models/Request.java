2d19ba5cddac030fe72a490f550e06acf1537e5d
Zakyyy <ahmed.abdelghafour@student.guc.edu.eg>
2019-01-21 16:41:29 +0200

    editing customer model

--- app/src/main/java/com/example/ostabelya/models/Request.java
+++ app/src/main/java/com/example/ostabelya/models/Request.java
@@ -1,13 +1,13 @@
 package com.example.ostabelya.models;
 
 public class Request {
-    public String uid;
+    public int uid;
     public int mid;
     public String requestDate;
     public String requestSlot;
     public String status;
 
-    public Request(String uid, int mid, String requestDate, String requestSlot, String status){
+    public Request(int uid, int mid, String requestDate, String requestSlot, String status){
         this.uid = uid;
         this.mid = mid;
         this.requestDate = requestDate;
